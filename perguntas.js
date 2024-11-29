eae5b55155b0b706422a8108d736a1c9bc81aad2
LordSailorOfic <00001088112560sp@al.educacao.sp.gov.br>
2024-11-29 12:02:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é seu mas as pessoas usam mais que você?',
+    'O seu nome!'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país tem no nome uma sílaba que dá para comer?',
+    'Japão!'
+)
+
+criaCartao(
+    'História',
+    'Quando o Brasil foi descoberto?',
+    'No verão, já não estava mais frio...'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "onde está o gato" em inglês?',
+    '"Where is the cat?"'
+)
